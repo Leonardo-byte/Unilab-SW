cb616612347cd4eaae7bc382efe7d99fdac703f4
Leonardo <ldmort123@gmail.com>
2026-06-22 04:25:38 -0500

    Actualizaciones secundarias

--- unilab/config/settings.py
+++ unilab/config/settings.py
@@ -9,7 +9,7 @@ class Settings:
     modules_path: str = "unilab.modules"
 
     # Configuración ESP32 serial
-    esp32_port: str = "COM3"        # Windows ejemplo
+    esp32_port: str = "/dev/ttyUSB0"  
     esp32_baudrate: int = 115200
     esp32_timeout: float = 1.0
     esp32_source: str = "esp32_01"
